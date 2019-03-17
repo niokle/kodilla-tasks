10cd1ce318e5e98951e6958f97df1e60a70ee198
Michał Kleszczewski <michalkleszczewski@macbook-pro-micha.home>
2019-03-17 17:23:49 +0100

    task 22.2 commit 1

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -1,10 +1,10 @@
 package com.crud.tasks.trello.client;
 
+import com.crud.tasks.config.TrelloConfig;
 import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -16,17 +16,9 @@ import java.util.Optional;
 
 @Component
 public class TrelloClient {
-    @Value("${trello.api.endpoint.prod}")
-    private String trelloApiEndpoint;
 
-    @Value("${trello.app.key}")
-    private String trelloAppKey;
-
-    @Value("${trello.app.token}")
-    private String trelloAppToken;
-
-    @Value("${trello.app.username}")
-    private String userName;
+    @Autowired
+    private TrelloConfig trelloConfig;
 
     @Autowired
     private RestTemplate restTemplate;
@@ -41,9 +33,9 @@ public class TrelloClient {
 
     public URI getUrl() {
         URI uri;
-        uri = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + userName + "/boards")
-                .queryParam("key", trelloAppKey)
-                .queryParam("token", trelloAppToken)
+        uri = UriComponentsBuilder.fromHttpUrl(trelloConfig.getTrelloApiEndpoint() + "/members/" + trelloConfig.getUserName() + "/boards")
+                .queryParam("key", trelloConfig.getTrelloAppKey())
+                .queryParam("token", trelloConfig.getTrelloAppToken())
                 .queryParam("fields", "name,id")
                 .queryParam("lists", "all")
                 .build().encode().toUri();
@@ -52,9 +44,9 @@ public class TrelloClient {
 
     public CreatedTrelloCard createNewCard(TrelloCardDto trelloCardDto) {
 
-        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/cards")
-                .queryParam("key", trelloAppKey)
-                .queryParam("token", trelloAppToken)
+        URI url = UriComponentsBuilder.fromHttpUrl(trelloConfig.getTrelloApiEndpoint() + "/cards")
+                .queryParam("key", trelloConfig.getTrelloAppKey())
+                .queryParam("token", trelloConfig.getTrelloAppToken())
                 .queryParam("name", trelloCardDto.getName())
                 .queryParam("desc", trelloCardDto.getDescription())
                 .queryParam("pos", trelloCardDto.getPos())
--- src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
+++ src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.trello.client;
 
+import com.crud.tasks.config.TrelloConfig;
 import com.crud.tasks.domain.TrelloBoardDto;
 import org.junit.Assert;
 import org.junit.Test;
@@ -7,51 +8,44 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 
-
-//@ActiveProfiles("test")
-//@RunWith(MockitoJUnitRunner.class)
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
 public class TrelloClientTest {
 
+    @InjectMocks
+    private TrelloClient trelloClient;
+
+    @Mock
+    private TrelloConfig trelloConfig;
+
     @Mock
     private RestTemplate restTemplate;
-
-    @InjectMocks
-    //@Autowired
-    private TrelloClient trelloClient;
 
     @Test
     public void testTrelloClient() {
         //given
         trelloClient = new TrelloClient();
-        //URI url = URI.create("https://api.trello.com/1/members/michakleszczewski/boards?key=a64377a3d776abed4f9f94f30a8ad114&token=32e7588fad441b9076c0d8696e7111e0758bd64cf3e3ab9135567530ea2284d9&fields=name,id");
+        Mockito.when(trelloConfig.getTrelloApiEndpoint()).thenReturn("http://test.com");
+        Mockito.when(trelloConfig.getTrelloAppKey()).thenReturn("test");
+        Mockito.when(trelloConfig.getTrelloAppToken()).thenReturn("test");
+        Mockito.when(trelloConfig.getUserName()).thenReturn("test");
 
         //when
         URI url = trelloClient.getUrl();
-        //System.out.println(url);
+
         Mockito.when(restTemplate.getForObject(url, TrelloBoardDto[].class)).thenReturn(null);
 
         //then
+        //TODO
         //trelloClient.getTrelloBoards().stream()
         //        .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getName()));
         Assert.assertEquals(0, trelloClient.getTrelloBoards().size());
         //Assert.assertEquals(1,1);
     }
-
-    //@Profile("test")
-    //@Configuration
-    //public class NameServiceTestConfiguration {
-    //    @Bean
-    //    @Primary
-    //    public RestTemplate restTemplate() {
-    //        return Mockito.mock(RestTemplate.class);
-    //    }
-    //}
 }
